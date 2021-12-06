a0e56011b5e969f1fa5d46dbf3496d47b037efcb
supruniiuk <juliasupruniuk23@gmail.com>
2021-12-07 01:56:43 +0200

    added client side

---
+++ frontend/src/app/components/drivers-page/drivers-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drivers-page',
+  templateUrl: './drivers-page.component.html',
+  styleUrls: ['./drivers-page.component.css']
+})
+export class DriversPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css'],
+})
+export class LoginPageComponent implements OnInit {
+  form: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      role: new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit() {}
+
+  submit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      console.log('Form: ', this.form);
+      const formData = { ...this.form.value };
+
+      console.log('Form Data:', formData);
+      if (location.pathname == '/login') this.login();
+    } else if (location.pathname == '/registration') {
+      this.registration();
+    }
+  }
+
+  login() {
+    console.log('login');
+  }
+  registration() {
+    console.log('registration');
+  }
+}
