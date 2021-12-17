15e2a924d3a81d09831636338a269be20b7c6ed6
supruniiuk <juliasupruniuk23@gmail.com>
2021-12-17 18:32:57 +0200

    start creating orders

---
+++ client/src/app/components/orders-page/create-order/create-order.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CarService, CarType } from 'src/app/shared/services/car.service';
+import { Order } from 'src/app/shared/services/orders.service';
+
+@Component({
+  selector: 'app-create-order',
+  templateUrl: './create-order.component.html',
+  styleUrls: ['./create-order.component.css'],
+})
+export class CreateOrderComponent implements OnInit {
+  newOrder: FormGroup;
+  carTypes: CarType[];
+  constructor(private carService: CarService) {}
+
+  ngOnInit(): void {
+    this.newOrder = new FormGroup({
+      origin_address: new FormControl('', [Validators.required]),
+      destination_address: new FormControl('', [Validators.required]),
+      number_of_people: new FormControl(null, [Validators.required]),
+      empty_trunk: new FormControl(false),
+      animals: new FormControl(false),
+      terminal: new FormControl(false),
+      air_condition: new FormControl(false),
+      car_type_id: new FormControl(null, [Validators.required]),
+    });
+
+    this.getCarTypes()
+  }
+
+  submit() {
+    console.log(this.newOrder.value);
+    if (this.newOrder.valid) {
+      const formData = { ...this.newOrder.value };
+      console.log(formData);
+    }
+  }
+
+  getCarTypes() {
+    this.carService.getAllCarTypes().subscribe(
+      (res) => {
+        this.carTypes = res;
+        console.log(this.carTypes);
+        console.log('success');
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+}
