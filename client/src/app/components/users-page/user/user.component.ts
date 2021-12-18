11daee9e4cdf0c0af5285bb64a785185a347c196
supruniiuk <juliasupruniuk23@gmail.com>
2021-12-18 15:55:35 +0200

    made users page more complex

---
+++ client/src/app/components/users-page/user/user.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UserInfo, UserService } from 'src/app/shared/services/user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+})
+export class UserComponent implements OnInit {
+  role: string = '';
+  user: UserInfo;
+  roles: FormGroup;
+
+  userRoles;
+
+  btnDisable: boolean = true;
+
+  ngOnInit(): void {
+    let pathname = location.pathname;
+    this.role = pathname.split('/')[1];
+    this.role = this.role.substring(0, this.role.length - 1);
+
+    this.roles = new FormGroup({
+      client: new FormControl(false),
+      driver: new FormControl(false),
+      dispatcher: new FormControl(false),
+    });
+
+    let href = location.pathname;
+    let id = href.split('/')[2];
+    this.getUser(id);
+  }
+
+  constructor(private userService: UserService) {}
+
+  getUser(id) {
+    this.userService.getUserById(id).subscribe(
+      (res: any) => {
+        this.user = res;
+        this.getUserRoles(this.user.user_id);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  getUserRoles(id) {
+    this.userService.getUserRoles(id).subscribe(
+      (res: any) => {
+        this.userRoles = res;
+        this.roles = new FormGroup({
+          client: new FormControl(res.client),
+          driver: new FormControl(res.driver),
+          dispatcher: new FormControl(res.dispatcher),
+        });
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  getNewRoles() {
+    let pickedRoles = this.roles.value;
+    let newRoles = {};
+    for (let role in pickedRoles) {
+      if (this.userRoles[role] != pickedRoles[role]) {
+        newRoles[role] = pickedRoles[role];
+      }
+    }
+
+    return newRoles;
+  }
+
+  checkUpdate() {
+    let newRoles = this.getNewRoles();
+
+    if (Object.keys(newRoles).length === 0) {
+      this.btnDisable = true;
+    } else {
+      this.btnDisable = false;
+    }
+  }
+
+  submit() {
+    let newRoles = this.getNewRoles();
+
+    for (let role in newRoles) {
+      if (newRoles[role]) {
+        this.userService.setUserRole(this.user.user_id, role).subscribe(
+          () => {
+            console.log('SUCCESS add');
+            this.userRoles[role] = newRoles[role];
+            this.checkUpdate();
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      } else {
+        this.userService.deleteUserRole(this.user.user_id, role).subscribe(
+          () => {
+            console.log('SUCCESS delete');
+            this.userRoles[role] = newRoles[role];
+            this.checkUpdate();
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      }
+    }
+    console.log(newRoles);
+  }
+
+  getUserOrder(id) {}
+}
---
+++ client/src/app/components/users-page/users-page.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { UserInfo, UserService } from 'src/app/shared/services/user.service';
+
+@Component({
+  selector: 'app-users-page',
+  templateUrl: './users-page.component.html',
+  styleUrls: ['./users-page.component.css'],
+})
+export class UsersPageComponent implements OnInit {
+  role: string = '';
+
+  errorMessage;
+  users: UserInfo[] = [];
+  page = 1;
+  pages = 0;
+  count = 0;
+  deleteUser: UserInfo = null;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    let pathname = location.pathname;
+    this.role = pathname.split('/')[1];
+    this.role = this.role.substring(0, this.role.length - 1);
+    console.log('this.role', this.role);
+    this.getUsers(this.page, this.role);
+  }
+
+  getUsers(page, role) {
+    this.userService.getUsers(page, role).subscribe(
+      (res: any) => {
+        this.count = res.count;
+        this.pages = Math.ceil(this.count / 10);
+        this.users = res.users;
+        console.log(this.pages);
+      },
+      (err) => {
+        this.errorMessage = err;
+      }
+    );
+  }
+
+  deleteUserById(id) {
+    this.userService.deleteUserById(id).subscribe(
+      () => {
+        this.users = this.users.filter((user) => user.user_id != id);
+        this.count -= 1;
+        console.log('success');
+      },
+      (err) => {
+        console.log(err);
+        this.errorMessage = err;
+      }
+    );
+  }
+}
