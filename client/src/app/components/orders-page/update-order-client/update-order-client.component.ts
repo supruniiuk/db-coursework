1fb25fa1dde568fa0ed53a690ccdc48ca4cefe0a
supruniiuk <juliasupruniuk23@gmail.com>
2021-12-19 18:38:40 +0200

    added client grading

---
+++ client/src/app/components/orders-page/update-order-client/update-order-client.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Order, OrderService } from 'src/app/shared/services/orders.service';
+
+@Component({
+  selector: 'app-update-order-client',
+  templateUrl: './update-order-client.component.html',
+  styleUrls: ['./update-order-client.component.css'],
+})
+export class UpdateOrderClientComponent implements OnInit {
+  gradeOrder: FormGroup;
+  path: string;
+  id: number;
+  order: Order;
+
+  gradeClose: boolean = false;
+
+  constructor(private orderService: OrderService) {}
+
+  ngOnInit(): void {
+    let href = location.pathname;
+    this.path = href.split('/')[3];
+    this.id = +href.split('/')[2];
+
+    this.gradeOrder = new FormGroup({
+      client_grade: new FormControl(5, [Validators.required]),
+      client_comment: new FormControl('', [Validators.required]),
+    });
+
+    this.getOrder(this.id);
+  }
+
+  getOrder(id) {
+    this.orderService.getOrderById(id).subscribe(
+      (res) => {
+        this.order = res;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  grade() {
+    if (this.gradeOrder.valid) {
+      const formData = { ...this.gradeOrder.value };
+      console.log(formData);
+      this.orderService.gradeOrderClient(this.id, formData).subscribe(
+        () => {
+          console.log('success');
+          this.gradeClose = true;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    }
+  }
+}
